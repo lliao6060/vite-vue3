6b1f1c59186a5b54bd8ff5c358992318dade0834
MOMODA1\FG000098 <meiliao1207@gmail.com>
2022-03-03 16:13:05 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, toRef, onUpdated, reactive } from 'vue'
+
+const add = () => {
+  count.value ++
+}
+
+const userInfo = reactive({
+  name: 'maju',
+  age: 5
+})
+
+const userName = toRef(userInfo, 'name') //將響應物件裡的屬性抽離出來 使用
+
+onUpdated(() => {
+  console.log('toRef:', userName.value)
+  console.log('元物件', userInfo.name)
+})
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="foo"></div>
+  <button type="button" @click="add">count is: {{ count }}</button>
+  <p>{{ userName }}</p>
+  <input v-model="userName"/>
+</template>
+
+<style lang="scss" scoped>
+a {
+  color: #42b983;
+}
+.foo {}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,86 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+const { resolve } = require('path')
+import legacy from '@vitejs/plugin-legacy'
+
+// https://vitejs.dev/config/
+
+export default ({ mode }) => {
+  return defineConfig({
+    // base: loadEnv(mode, process.cwd()).VITE_PUBLIC,
+    base: "./",
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        '@components': resolve(__dirname, 'src/components'),
+        '/images': 'src/assets/images'
+      },
+    },
+    //跨域設定
+    server: {
+      host: 'localhost',
+      port: 3000,
+      open: false,
+      https: false,
+      proxy: {
+        // 字串寫法
+        '/foo': 'http://localhost:4567',
+        // 選項寫法
+        '/api': {
+          target: 'http://jsonplaceholder.typicode.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        },
+        // 正則寫法
+        '^/fallback/.*': {
+          target: 'http://jsonplaceholder.typicode.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/fallback/, '')
+        },
+      }
+    },
+    //打包模式 & MPA多頁應用
+    build: {
+      assetsDir: "./static",
+      chunkSizeWarningLimit: 1500, //分塊打包 大塊分解成更小的塊
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          // arcticle: resolve(__dirname, 'arcticle/index.html')
+        },
+        output: {
+          manualChunks(id) {
+            if(id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          }
+        },
+        //預設會有hash 如果想要保留原檔名想要取消檔名的hash時 如下
+        // output: {
+        //   entryFileNames: `assets/[name].js`,
+        //   chunkFileNames: `assets/[name].js`,
+        //   assetFileNames: `assets/[name].[ext]`
+        // }
+      }
+    },
+    //全局都可以引用
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @import "@/assets/styles/_mixin.scss";
+          @import "@/assets/styles/_palette.scss";
+        `
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      //提供傳統瀏覽器兼容
+      legacy({
+        targets: ['ie >= 11'],
+        additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+      })
+    ],
+  })
+}
