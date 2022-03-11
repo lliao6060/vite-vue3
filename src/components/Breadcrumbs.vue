bff628b67948168e8afab8b3cdbb742aba6f390a
MOMODA1\FG000098 <meiliao1207@gmail.com>
2022-03-11 13:38:23 +0800

    bread add last child active

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -7,40 +7,83 @@
     list: []
   })
 
-
   onMounted(() => {
     let matched = route.matched
     state.list = matched
   })
 
-  watch(() => route.matched, (nV, oV) => {
+  watch(() => route.matched, (nV) => {
     let matched = nV
-    state.list = matched
+    state.list = matched.map((crumb) => {
+      const { name, path, meta } = crumb
+      return {
+        name,
+        path,
+        title: meta.title
+      }
+    })
   })
+
 </script>
 
 <template>
-  <div>
-    <ul>
-      <li>
-        <a 
-          v-for="(item, i) in state.list" 
-          :key="`item-${i}`"
-        >
-          <router-link :to="{ path: item.path }">
-            <template v-if="item.path === '/index'">
-              {{ item.meta.title }}
-            </template>
-            <template v-else>
-              首頁 > {{ item.meta.title }}
-            </template>
-          </router-link>
-        </a>
+  <div class="breadcrumb">
+    <ul class="breadcrumb__items">
+      <li         
+        v-for="(crumb, i) in state.list" 
+        :key="`item-${i}`"
+        class="bread-item"
+      >
+        <template v-if="crumb.path === '/index'">
+          <a>
+            <router-link to="/index">首頁</router-link>
+          </a>
+        </template>
+        <template v-else>
+          <a class="bread-array">
+            <router-link to="/index">首頁</router-link>
+            <router-link :to="{ path: crumb.path }">&nbsp;{{ crumb.title }}</router-link>
+          </a>
+        </template>
       </li>
     </ul>
   </div>
 </template>
 
-<style>
+<style lang="scss">
+  .breadcrumb {
+    &__items {
+      @include flex;
+      >li {
+        cursor: pointer;
+        a {
+          text-decoration: none;
+          color: #222;
+          &.bread-array {
+            margin-right: 5px;
+            >a {
+              &:last-child {
+                font-weight: bold;
+                &::before {
+                  font-weight: 300;
+                }
+              }
+              &:not(:first-child) {
+                margin-left: 5px;
+                &::before {
+                  content: '>';
+                }
+              }
+              &:not(:last-child) {
+                &:hover {
+                  opacity: 0.7;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 
 </style>
